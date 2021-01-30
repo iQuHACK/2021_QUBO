4ebc11b0c45fba4ce556bca1697f0af5324e0fad
Sebastien Lohier <gfexatschool@gmail.com>
2021-01-30 19:52:56 +0000

    First Run

--- nsp2gc.py
+++ nsp2gc.py
@@ -1,3 +1,5 @@
+from dimod import DiscreteQuadraticModel
+from dwave.system import LeapHybridDQMSampler
 nurses = 3
 days = 3
 shifts = 3
@@ -36,5 +38,28 @@ if __name__ == "__main__":
     adj_nodes, adj_edges = adj_to_nodes_and_edges(adj)
     print("Nodes: ", adj_nodes)
     print("Edges: ", adj_edges)
+    
+for i in adj_nodes:
+  dqm.add_variable(N, label=i)
+for i in adj:
+  dqm.set_linear(p, range(N))
+for i0,i1 in adj_edges:
+    dqm.set_quadratic(i0,i1, {(c,c): lagrange for c in range(N)})
+    
+sampler= LeapHybridDQMSampler()
+sampleset= sampler.sample_dqm(dqm)
+sample=sampleset.first.sample
+energy = sampleset.first.energy
+valid = True
+    for edge in adj_edges:
+        i, j=edge
+        if sample[i]== sample[j]:
+            valid = False
+            break
+    print("Solution: ", sample)
+    print("Solution energy:", energy)
+    print("Solution validity: ", valid)
+            
+
     
     
