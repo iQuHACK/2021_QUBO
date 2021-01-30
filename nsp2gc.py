1802a7ab56adbe270f591e21eac77678a25ec399
Alexandra Dima <adadima@mit.edu>
2021-01-30 19:44:02 +0000

    Started implementing  reduction from nurrse scheduling to graph coloring

---
+++ nsp2gc.py
@@ -0,0 +1,40 @@
+nurses = 3
+days = 3
+shifts = 3
+
+
+def nsp_to_graph_coloring(nurses, days, shifts):
+    adj = {}
+    for d in range(days):
+        for s in range(shifts):
+            adj[f"d{d}_s{s}"] = set()
+            
+    for d in range(days):
+        for s in range(shifts):
+            adj[f"d{d}_s{s}"] = {f"d{d}_s{(s + 1) % shifts}", f"d{d}_s{(s + 2) % shifts}"}
+            if s == shifts - 1 and d < days - 1:
+                adj[f"d{d}_s{s}"].add(f"d{d+1}_s0")
+    
+    return adj
+
+def adj_to_nodes_and_edges(adj):
+    nodes = list(adj.keys())
+    edges = []
+    for source, destinations in adj.items():
+        for dest in destinations:
+            edges.append((source, dest))
+    return nodes, edges
+
+if __name__ == "__main__":
+    print("Number of nurses: ", nurses)
+    print("Number of days: ", days)
+    print("Shifts: ", list(range(shifts)))
+    
+    adj = nsp_to_graph_coloring(nurses, days, shifts)
+    print("Adjacency list: ", adj)
+    
+    adj_nodes, adj_edges = adj_to_nodes_and_edges(adj)
+    print("Nodes: ", adj_nodes)
+    print("Edges: ", adj_edges)
+    
+    
